b3595f554b3493f2feaf41f36dfee5a3fece5d67
Patrick Morpey <patrick@morphp.nl>
2023-11-30 16:34:50 +0100

    Create directory first

--- src/RootBundleManager.php
+++ src/RootBundleManager.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Medas\HttpClient;
 
 use Medas\Core\Attributes\{ConfigValue, Service};
+use Medas\Core\Interfaces\DirectoryManager;
 
 #[Service]
 readonly class RootBundleManager
 {
     public function __construct(
         #[ConfigValue(ConfigOptions\RootBundleDirectory::class)]
-        private string $directory,
+        private string           $directory,
+
+        private DirectoryManager $directoryManager,
     )
     {
     }
@@ -21,6 +24,7 @@ readonly class RootBundleManager
         $filename = $this->directory . '/cacert.pem';
 
         if (!file_exists($filename)) {
+            $this->directoryManager->create($filename);
             file_put_contents($filename, file_get_contents('https://curl.haxx.se/ca/cacert.pem'));
         }
 
